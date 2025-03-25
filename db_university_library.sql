851aead0acdb38f6598c0302e58a0b7dbdea28a4
Fabio Masciarelli <158151713+FabioMasciarelli@users.noreply.github.com>
2025-03-25 11:27:31 +0100

    first commit

---
+++ db_university_library.sql
@@ -0,0 +1,56 @@
+CREATE DATABASE university_library;
+USE university_library;
+
+CREATE TABLE Categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    category_name VARCHAR(50) NOT NULL,
+    category_description TEXT
+);
+
+CREATE TABLE Authors (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(50) NOT NULL,
+    author_surname VARCHAR(50) NOT NULL,
+    author_biography TEXT,
+    birthday_date DATE
+);
+
+CREATE TABLE Books (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    isbn VARCHAR(13) UNIQUE NOT NULL,
+    author VARCHAR(50),
+    year_of_publication INT,
+    isbn_id INT UNIQUE,
+    category_id INT,
+    FOREIGN KEY (isbn_id) REFERENCES ISBN(id),
+    FOREIGN KEY (category_id) REFERENCES Categories(id)
+);
+
+CREATE TABLE Books_Authors (
+    book_id INT,
+    author_id INT,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES Books(id),
+    FOREIGN KEY (author_id) REFERENCES Authors(id)
+);
+
+CREATE TABLE Students (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    student_name VARCHAR(50) NOT NULL,
+    student_surname VARCHAR(50) NOT NULL,
+    student_email VARCHAR(100) UNIQUE,
+    student_phone_number VARCHAR(15),
+    registration_date DATE DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE Loans (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    book_id INT,
+    student_id INT,
+    loan_date DATE NOT NULL,
+    return_date DATE,
+    loan_status ENUM('active', 'concluded', 'late') DEFAULT 'active',
+    FOREIGN KEY (book_id) REFERENCES Books(id),
+    FOREIGN KEY (student_id) REFERENCES Students(id)
+);
